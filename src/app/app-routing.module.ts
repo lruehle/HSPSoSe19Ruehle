9ae55814438ba1074a9df6a1c2bb12e93dbb52ec
lruehle <35702288+lruehle@users.noreply.github.com>
2019-08-18 17:26:15 +0200

    components & Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {DetailKursComponent} from './components/detail-kurs/detail-kurs.component';
+import {DetailNewsComponent} from './components/detail-news/detail-news.component';
+import {DetailTopkursComponent} from './components/detail-topkurs/detail-topkurs.component';
+import {OverviewCoursCategoryComponent} from './components/overview-cours-category/overview-cours-category.component';
+import {OverviewCoursesOfferedComponent} from './components/overview-courses-offered/overview-courses-offered.component';
+import {OverviewWeekComponent} from './components/overview-week/overview-week.component';
+import {OverviewWeekdayComponent} from './components/overview-weekday/overview-weekday.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home' , component: HomeComponent},
+  {path: 'DetailCourse', component: DetailKursComponent},
+  {path: 'DetailNews', component: DetailNewsComponent},
+  {path: 'DetailTopCourse', component: DetailTopkursComponent},
+  {path: 'OverviewCategories', component: OverviewCoursCategoryComponent},
+  {path: 'OverviewOffered', component: OverviewCoursesOfferedComponent},
+  {path: 'OverviewWeek', component: OverviewWeekComponent},
+  {path: 'OverviewWeekDay', component: OverviewWeekdayComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetailNewsComponent } from './components/detail-news/detail-news.component';
+import { DetailTopkursComponent } from './components/detail-topkurs/detail-topkurs.component';
+import { DetailKursComponent } from './components/detail-kurs/detail-kurs.component';
+import { OverviewCoursCategoryComponent } from './components/overview-cours-category/overview-cours-category.component';
+import { OverviewCoursesOfferedComponent } from './components/overview-courses-offered/overview-courses-offered.component';
+import { OverviewWeekComponent } from './components/overview-week/overview-week.component';
+import { OverviewWeekdayComponent } from './components/overview-weekday/overview-weekday.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailNewsComponent,
+    DetailTopkursComponent,
+    DetailKursComponent,
+    OverviewCoursCategoryComponent,
+    OverviewCoursesOfferedComponent,
+    OverviewWeekComponent,
+    OverviewWeekdayComponent
   ],
   imports: [
     BrowserModule,
