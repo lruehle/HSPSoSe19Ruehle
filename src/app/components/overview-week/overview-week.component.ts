545585e8720306d6ca4f06f2a869292b6fa942b4
lruehle <35702288+lruehle@users.noreply.github.com>
2019-09-30 00:15:05 +0200

    midnight push
    shortly past midnight

--- src/app/components/overview-week/overview-week.component.ts
+++ src/app/components/overview-week/overview-week.component.ts
@@ -8,15 +8,44 @@ import {HttpClient} from '@angular/common/http';
 })
 export class OverviewWeekComponent implements OnInit {
 
-  public weekday;
+  public monday;
+  public tuesday;
+  public wednesday;
+  public thursday;
+  public friday;
+  public saturday;
+  public sunday;
   public id;
+  public course;
   constructor(private http: HttpClient) {
-    this.weekday = [];
+    this.course = 36;
+    this.monday = [];
+    this.tuesday = [];
+    this.wednesday = [];
+    this.thursday = [];
+    this.friday = [];
+    this.saturday = [];
+    this.sunday = [];
 }
 
   ngOnInit() {
-    this.http.get('https://hochschulsport-koeln.de/json/courses/' + this.id + '/?_format=json')
-      .subscribe((data) => {this.weekday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 8 + '/?_format=json')
+      .subscribe((data) => {this.monday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 9 + '/?_format=json')
+      .subscribe((data) => {this.tuesday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 10 + '/?_format=json')
+      .subscribe((data) => {this.wednesday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 11 + '/?_format=json')
+      .subscribe((data) => {this.thursday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 12 + '/?_format=json')
+      .subscribe((data) => {this.friday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 13 + '/?_format=json')
+      .subscribe((data) => {this.saturday = data; });
+    this.http.get('https://hochschulsport-koeln.de/json/coursesbyweekday/' + 14 + '/?_format=json')
+      .subscribe((data) => {this.sunday = data; });
+  }
+  show(i) {
+   this.course = i;
   }
 
 }
